7223c27b3d21ce5df96f17f591c6e2135e22f77e
wode <38369557+Lushilong123@users.noreply.github.com>
2023-02-07 07:30:15 +0800

    Add files via upload

---
+++ Gorgeous book template/#delete-temp-files.bat
@@ -0,0 +1,5 @@
+@echo off
+del /q *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.ptc*.vrb *.synctex(busy)
+del /q *.ptc *.atfi *.pre
+
+del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
