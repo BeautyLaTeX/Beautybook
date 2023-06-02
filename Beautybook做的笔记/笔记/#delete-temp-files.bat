9c725528d92105e8cca249b24968f0772b71aa94
Ethan <38369557+BeautyLaTeX@users.noreply.github.com>
2023-06-02 14:01:35 +0800

    Add files via upload

---
+++ Beautybook做的笔记/笔记/#delete-temp-files.bat
@@ -0,0 +1,5 @@
+@echo off
+del /q *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.ptc*.vrb *.synctex(busy)
+del /q *.ptc
+
+del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
